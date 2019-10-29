df8e0ac57f37a4f7d66d8313937f0ff4599f09df
Adamaya <mail.adamyas@gmail.com>
2019-10-29 18:14:56 +0530

    Update crd.py

--- crd.py
+++ crd.py
@@ -1,3 +1,5 @@
+# Change the credential according to your details
+
 def data():
     crd = {"from": "--------------@gmail.com", "password": "----------", "to": "--------------@gmail.com"}
     return crd
